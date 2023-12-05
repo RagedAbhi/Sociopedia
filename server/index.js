20c946e75771e29c274921dd36b5036440421006
Abhilash Khadanga <117280328+RagedAbhi@users.noreply.github.com>
2023-12-06 00:15:30 +0530

    try2

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js"
 
 // CONFIGURATIONS
-app.use(cors())
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-dotenv.config()
 const app = express()
+app.use(cors())
+dotenv.config()
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
