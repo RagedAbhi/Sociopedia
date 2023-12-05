bb374ab59d6c8c1267845e2f6947a9bcfce45061
Abhilash Khadanga <117280328+RagedAbhi@users.noreply.github.com>
2023-12-05 23:21:55 +0530

    cors disable

--- server/index.js
+++ server/index.js
@@ -22,11 +22,11 @@ import { users, posts } from "./data/index.js"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 dotenv.config()
-app.use(cors({
-    origin: "https://sociopedia-kappa.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "https://sociopedia-kappa.vercel.app",
+//     methods: ["GET", "POST"],
+//     credentials: true
+// }))
 const app = express()
 app.use(express.json())
 app.use(helmet())
